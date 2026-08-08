e8ce29a38ffaab49c0a0d81f3c96a0913c1e6040
sunjuzhong <129019668+sunjuzhong@users.noreply.github.com>
2026-08-08 10:59:56 +0800

    refactor(tutorials): remove T02 mesh upload lesson

--- web/src/pages/TutorialPage.tsx
+++ web/src/pages/TutorialPage.tsx
@@ -21,7 +21,6 @@ import type { Flow360Status } from '../api/client'
 import { api } from '../api/client'
 import TopBar from '../components/TopBar'
 import TutorialEnvironmentBuilder from '../components/TutorialEnvironmentBuilder'
-import T02TutorialPage from './T02TutorialPage'
 import T03TutorialPage from './T03TutorialPage'
 import T04TutorialPage from './T04TutorialPage'
 import {
@@ -72,7 +71,6 @@ function downloadJSON(params: Record<string, unknown>, alpha: number) {
 
 export default function TutorialPage() {
   const { tutorialId = '' } = useParams()
-  if (tutorialId.toUpperCase() === 'T02') return <T02TutorialPage />
   if (tutorialId.toUpperCase() === 'T03') return <T03TutorialPage />
   if (tutorialId.toUpperCase() === 'T04') return <T04TutorialPage />
   if (tutorialId.toUpperCase() !== 'T01') return <Navigate to="/tutorials" replace />
